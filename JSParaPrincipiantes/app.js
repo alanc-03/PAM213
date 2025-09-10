5e181ba3442a18f5071d56807800b5a22ac6b61a
alanc-03 <124049941@upq.edu.mx>
2025-09-10 15:33:33 -0600

    operadores 1
    se crean operaciones sencillas

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,30 +1,6 @@
-//este codigo hace lo siguiente
+let numberone = 60;
+let numbertwo = 100;
 
-var nameuser = "jhon";
-let lastname = "carter";
+let resut = numberone + numbertwo;
 
-//reasignando el valor de la vvariable nameuser
-
-nameuser = 'pepe';
-
-//cualquiera
-
-const PI = 3.1416;
-PI = 100
-
-console.log(nameuser)
-
-// camelcase
-//sdhfsggoikfsfermsalsdmlkmv
-let userName = 'gordon';
-
-/*el siguiente comentario esta ocupando multiples lineas
-fgsdf
-sfh
-htyj
-
-kgh
-j
-
-stht
-*/
+console.log(resut);
