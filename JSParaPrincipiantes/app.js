045ddfb9077bf35decf4827fd462b46cadfc71f9
alanc-03 <124049941@upq.edu.mx>
2025-09-10 16:18:25 -0600

    bucles 1.0
    se usa while()
    para repetir operaciones

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,41 +1,7 @@
-/*let passwordDB = 'pepe123'
 
-let input = 'pepe'
-
-let result = input == passwordDB;
-
-if(result == true){
-    console.log('login correcto');
-}else{
-    console.log('login incorrecto');
-}
-
-if(result == false){
-    console.log('login incorrecto');
-}
-
-
-
-console.log(result);
-
-let score = 70;
-if(score > 30) {
-    console.log('you need to practice more')
-}else if(score > 15) {
-    console.log('estas mejorando')
-}else{
-    console.log('you need to follow this tutorial')
-}*/
-
-let typrCard = 'sdcmlsdmvlsdaclsd';
-
-switch(typrCard){
-    case 'Debit Card':
-        console.log('this is a credit card');
-            break;
-    case 'Credit Card':
-        console.log('this is a credit card');
-            break;
-    default:
-        console.log('no Card');
+let count = 1;
+while(count <= 50){
+    console.log(count);
+    count = count + 1;
+//    count++;
 }
