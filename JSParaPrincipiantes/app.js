669315528218cbf9185b9d1cdd73e20e5361a43b
alanc-03 <124049941@upq.edu.mx>
2025-09-10 15:29:50 -0600

    variables 1.2
    buena interpretacion para nombres de variables, los comentarios no se leen por el programa

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,9 +1,30 @@
+//este codigo hace lo siguiente
+
 var nameuser = "jhon";
 let lastname = "carter";
 
+//reasignando el valor de la vvariable nameuser
+
 nameuser = 'pepe';
+
+//cualquiera
 
 const PI = 3.1416;
 PI = 100
 
 console.log(nameuser)
+
+// camelcase
+//sdhfsggoikfsfermsalsdmlkmv
+let userName = 'gordon';
+
+/*el siguiente comentario esta ocupando multiples lineas
+fgsdf
+sfh
+htyj
+
+kgh
+j
+
+stht
+*/
