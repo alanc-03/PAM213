79fd8960fb47a9cdc2c42ec5794f4294945ec197
alanc-03 <124049941@upq.edu.mx>
2025-09-10 14:52:15 -0600

    pruebas de objetos 1

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,1 +1,34 @@
-document.write("<h1>hola mundo</h1>");
+//tipos de datos
+/*"Hello Word" //string
+'Hello word' //string
+
+
+
+1000000 // number
+
+-2.3
+
+//boolean
+true
+false
+
+//array
+['joe', 'ryan', 'martha']
+[1,2,3]
+[true,false,true,false]
+
+//object
+
+'ryan'
+70.4
+14
+true
+
+{
+    "username": 'ryan',
+    "score": 70.4,
+    "hours": 14,
+    "profesional": true
+}
+*/
+console.log([1,2,3,4]);
