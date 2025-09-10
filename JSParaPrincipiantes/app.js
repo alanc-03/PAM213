fbf249493905b0348f21109ef49eddf4a5f919c1
alanc-03 <124049941@upq.edu.mx>
2025-09-10 15:12:09 -0600

    variables 1
    se borra y se declaran variables simples

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,34 +1,4 @@
-//tipos de datos
-/*"Hello Word" //string
-'Hello word' //string
+var nameuser = "jhon";
+let lastname = "carter";
 
-
-
-1000000 // number
-
--2.3
-
-//boolean
-true
-false
-
-//array
-['joe', 'ryan', 'martha']
-[1,2,3]
-[true,false,true,false]
-
-//object
-
-'ryan'
-70.4
-14
-true
-
-{
-    "username": 'ryan',
-    "score": 70.4,
-    "hours": 14,
-    "profesional": true
-}
-*/
-console.log({"username": "ryan","score": 70.4});
+console.log(lastname)
