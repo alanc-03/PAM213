d328a61dd930ae67bd929191bdc5f4429edf3965
alanc-03 <124049941@upq.edu.mx>
2025-09-10 17:31:29 -0600

    funciones 1.1
    usamos llamada a la funcion para sumar diferentes numeros

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,5 +1,5 @@
 
-function saludo(Persona){
+/*function saludo(Persona){
 //    console.log(Persona);
     console.log('Hola' +' '+ Persona);
 }
@@ -9,4 +9,17 @@ saludo('Ezequiel');
 saludo('Adrian');
 saludo('josue');
 saludo('jean');
-saludo('amanda');
+saludo('amanda');*/
+
+
+
+function add(n1, n2){
+    console.log(n1 + n2);
+}
+
+add(130,360);
+add(10,60);
+add(0,6);
+add(1,3);
+add(13,60);
+add(8,9);
