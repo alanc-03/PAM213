8ba67d49b865d302d2b088b508a74cc0ca6a4a38
alanc-03 <124049941@upq.edu.mx>
2025-09-10 15:43:20 -0600

    operadores 1.2
    comparacion de numeros

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,6 +1,15 @@
-let name = 'jhon';
+/*let name = 'jhon';
 let lastname = 'carter';
 
 let completname = name + ' '+ lastname;
 
-console.log(completname);
+console.log(completname);*/
+
+
+
+let numberOne = 100;
+let numberTwo = 500;
+
+let result = numberOne != numberTwo;
+
+console.log(result);
