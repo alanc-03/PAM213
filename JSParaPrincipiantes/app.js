4ae4a5e6241221997232af112c62ab6bed4fc756
alanc-03 <124049941@upq.edu.mx>
2025-09-10 16:38:21 -0600

    bucles 1.1
    utilizamos el for
    como sustituto del while
    para repetir acciones

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,7 +1,19 @@
 
-let count = 1;
+/*let count = 1;
 while(count <= 50){
     console.log(count);
     count = count + 1;
 //    count++;
+}*/
+
+let names = ['ryan','joe','jhon'];
+
+/*
+console.log(names[1]);
+console.log(names.length);
+*/
+//for(let i= 0 ;i< names.length-1; i--)
+for(let i= names.length-1; i >= 0 ; i--){
+    console.log(names[i])
+
 }
