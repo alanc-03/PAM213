5ad5dbd0e0c357b3d219ab821a26fbf168b13b44
alanc-03 <124049941@upq.edu.mx>
2025-09-10 17:24:44 -0600

    funciones 1.0
    usamos una funxion censilla y la reutilizamos para escribir diferentes nombres

--- JSParaPrincipiantes/app.js
+++ JSParaPrincipiantes/app.js
@@ -1,19 +1,12 @@
 
-/*let count = 1;
-while(count <= 50){
-    console.log(count);
-    count = count + 1;
-//    count++;
-}*/
-
-let names = ['ryan','joe','jhon'];
-
-/*
-console.log(names[1]);
-console.log(names.length);
-*/
-//for(let i= 0 ;i< names.length-1; i--)
-for(let i= names.length-1; i >= 0 ; i--){
-    console.log(names[i])
-
+function saludo(Persona){
+//    console.log(Persona);
+    console.log('Hola' +' '+ Persona);
 }
+
+
+saludo('Ezequiel');
+saludo('Adrian');
+saludo('josue');
+saludo('jean');
+saludo('amanda');
