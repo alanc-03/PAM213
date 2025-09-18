29c919a5e29b66844b721d35dfd41affffe2fed9
alanc-03 <124049941@upq.edu.mx>
2025-09-17 22:08:39 -0600

    arrow/array
    se hace una funcion arrow que rregrese un array

---
+++ practica1/Arrow02.js
@@ -0,0 +1,10 @@
+
+let SaludoPersonalizado = (nombre , edad) =>{
+
+
+    const saludo ="Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+
+    return saludo;
+}
+
+console.log(SaludoPersonalizado("emilia",3));
