1e0a0448c0910a14b2c2b7ff2fff724998d9ee8c
alanc-03 <124049941@upq.edu.mx>
2025-09-23 15:40:33 -0600

    ejercicio a
    se completa el código para mostrar nombre, edad y ciudad

---
+++ practica2/preactica2.js
@@ -0,0 +1,14 @@
+
+const persona ={
+    nombre: "alan cruz",
+    edad: 33,
+    direccion:{
+        ciudad:"Tequisquiapan",
+        pais:"Mexico"
+    }
+};
+
+const {nombre,edad,direccion}=persona;
+
+console.log("Me llamo "+nombre+", tengo "+edad+" años y vivo en "+direccion.ciudad);
+
