ffe5dc24f661a84cb495fa763cae93cd3bd0d454
alanc-03 <124049941@upq.edu.mx>
2025-09-23 17:28:54 -0600

    ejercicio C
    se acomoda el codigo para mostrar
    una persona en especifico,
    los que tienen su misma edad,
    y las suma total de las edades.

--- practica2/C.js
+++ practica2/C.js
@@ -1,2 +1,19 @@
 
-const
+const personas =[
+    {nombre:"Ana", edad:22},
+    {nombre:"Luis", edad:35},
+    {nombre:"Angel", edad:35},
+    {nombre:"Maria", edad:28},
+    {nombre:"Marcos", edad:25}
+];
+
+const busqueda = personas.find(elemento=>elemento.nombre=="Luis");
+
+console.log(busqueda);
+personas.forEach(persona => {if(persona.edad===35){
+    console.log(persona.nombre + " tiene " + persona.edad + "años.")
+}}
+);
+
+let sumaT = personas.reduce((sum,persona)=>sum + persona.edad,0);
+console.log("la suma de las edade es: "+ sumaT);
