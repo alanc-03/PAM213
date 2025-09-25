cb10ac532a04cbbb197378cbed9c20849da9b6ef
alanc-03 <124049941@upq.edu.mx>
2025-09-25 17:36:19 -0600

    ejercicio a
    se crea la funcion resta y se importa al main.

---
+++ practica3/main.js
@@ -0,0 +1,8 @@
+import { resta } from "./Utils.js";
+
+console.log(resta(5, 9));   // -4
+console.log(resta(1, 6));   // -5
+console.log(resta(5, 5));   // 0
+console.log(resta(3, 1));   // 2
+console.log(resta(1, 1));   // 0
+console.log(resta(89, 11)); // 78
